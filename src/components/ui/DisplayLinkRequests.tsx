99e81399e74f5d072b006a29ab549a3bdaacd5dd
Miguel Fasanella <miguellfasanellap@gmail.com>
2024-03-01 21:32:34 -0400

    nuevamente antes de cagarla

---
+++ src/components/ui/DisplayLinkRequests.tsx
@@ -0,0 +1,73 @@
+import { useState, type FC } from "preact/compat";
+
+import type { IPetInfo, IUser } from "@/types/User";
+import { useStore } from "@nanostores/preact";
+import { $tasks, $updateTasks } from "@/stores/Loading";
+import { useNotifications } from "@/hooks/useNotifications";
+import { PromiseConfirmHelper } from "@/stores/Notifications";
+import "./DisplayLinkRequest.css";
+import { ModalButton } from "../hoc/ModalButton";
+import { PetInfo } from "./PetInfo";
+import type { IResponseLinkRequest } from "@/types/LinkRequest";
+import { responseLinkRequest } from "@/database/DbLink";
+import { $responseLinkRequest } from "@/stores/UserInfo";
+
+interface Props {
+    userInfo: IUser;
+}
+
+export const DisplayLinkRequests: FC<Props> = ({ userInfo }) => {
+
+    const tasks = useStore($tasks);
+
+    const [alreadyDisplayed, setAlreadyDisplayed] = useState(false);
+    const [disappear, setDisappear] = useState("");
+
+    const { createNotification } = useNotifications();
+
+    const handleResponse = async (resInfo: IResponseLinkRequest & { petInfo: IPetInfo }) => {
+        setAlreadyDisplayed(true);
+        let notId = createNotification({
+            type: (resInfo.response === "accept") ? "info" : "warning",
+            content: `¿${(resInfo.response === "accept") ? "Aceptar" : "Rechazar"} la solicitud para ser dueño de ${resInfo.petInfo.name}?`,
+            confirmation: true,
+        });
+
+        let accepted = await PromiseConfirmHelper(notId);
+
+        if (!accepted) return;
+
+        $updateTasks("Respondiendo solicitud");
+        const res = await responseLinkRequest({ id: resInfo.id, response: resInfo.response });
+        $updateTasks("Respondiendo solicitud");
+
+        setDisappear(`link-request-${resInfo.id}`);
+        !res.error && setTimeout(() => $responseLinkRequest(resInfo.id, resInfo.petInfo, resInfo.response), 450);
+
+        createNotification({ type: res.error ? "error" : (resInfo.response === "accept") ? "success" : "info", content: res.message[0] });
+    };
+
+    return (
+        <section class="modal-child-container request-main-container">
+            {
+                userInfo.linkRequests.map((req, index) => (
+                    <div className={`request-bar ${(!alreadyDisplayed) ? "request-slide-in" : ""} ${disappear === `link-request-${req.id}` ? "fade-out-request" : ""}`} style={{ animationDelay: `${Number(index) * 200}ms` }} id={`link-request-${req.id}`}>
+                        {
+                            <div className="bar-img-container">
+                                <img src={req.requestingUser.image || "/default-image.png"} alt={`Solicitud de ${req.requestingUser.name}`} />
+                            </div>
+                        }
+                        <span class="bar-title">
+                            ¡{req.requestingUser.name} quiere enlazarte con <ModalButton children={<PetInfo pet={req.askedPet} />} textButton={req.askedPet.name} textTitle={req.askedPet.name} round full />!
+                            <div style={{ display: "flex", columnGap: "1rem" }}>
+                                <button disabled={tasks.includes("Respondiendo solicitud")} className="button" style={{ backgroundColor: "var(--success-color)", color: "#fff", padding: ".4rem .8rem", borderRadius: "5rem" }} onClick={() => handleResponse({ id: req.id, petInfo: req.askedPet, response: "accept" })}>Aceptar</button>
+                                <button disabled={tasks.includes("Respondiendo solicitud")} className="button" style={{ backgroundColor: "var(--error-color)", color: "#fff", padding: ".4rem .8rem", borderRadius: "5rem" }} onClick={() => handleResponse({ id: req.id, petInfo: req.askedPet, response: "reject" })}>Rechazar</button>
+                            </div>
+                        </span>
+
+                    </div>
+                ))
+            }
+        </section>
+    );
+};
---
+++ src/types/LinkRequest.ts
@@ -0,0 +1,65 @@
+import * as z from 'zod';
+
+export const ZResponseLinkRequest = z.object({
+    id: z.number({ required_error: 'El id de solicitud es requerido' }),
+    response: z.enum(["accept", "reject"], { invalid_type_error: "La respuesta no es válida", required_error: "La respuesta es requerida" }),
+});
+
+export type IResponseLinkRequest = z.infer<typeof ZResponseLinkRequest>;
+
+const PetBehaviorEnum = z.enum([
+    "funny",
+    "happy",
+    "angry",
+    "eater",
+    "dramatic",
+    "eager",
+], {
+    invalid_type_error: 'Una característica de mascota no es válida',
+});
+
+const PetTypeEnum = z.enum([
+    "dog",
+    "cat",
+    "horse",
+    "rabbit",
+    "monkey",
+    "turtle",
+    "goat",
+    "bird",
+    "pig",
+    "fish",
+    "hedgehog",
+    "other",
+], {
+    invalid_type_error: 'El tipo de mascota no es válido',
+});
+
+const ZPetInfo = z.object({
+    id       : z.number({ required_error: 'El id de la mascota es requerido' }),
+    name     : z.string({ required_error: 'El nombre es requerido', invalid_type_error: 'El nombre debe ser texto' }).trim().min(3).max(20),
+    petType  : PetTypeEnum,
+    image    : z.union([z.string({ invalid_type_error: 'La imagen debe ser un texto' }), z.null()]),
+    behaviors: z.array(PetBehaviorEnum).min(1, 'Tu mascota debe tener al menos un atributo'),
+});
+
+const ZUserInfo = z.object({
+    id   : z.number({ required_error: 'El id de usuario es requerido' }),
+    image: z.union([z.string({ invalid_type_error: 'La imagen debe ser un texto' }), z.null()]),
+    name : z.string({ required_error: 'El nombre es requerido', invalid_type_error: 'El nombre debe ser texto' }).trim().min(2, 'El nombre es muy corto'),
+});
+
+export const ZLinkRequest = z.object({
+    id            : z.number({ required_error: 'El id de solicitud es requerido' }),
+    askedPet      : ZPetInfo,
+    requestingUser: ZUserInfo,
+});
+
+export type ILinkRequest = z.infer<typeof ZLinkRequest>;
+
+export const ZNewLinkRequest = z.object({
+    userId: z.string().pipe(z.number({ invalid_type_error: "El id no es válido", required_error: "El id es requerido" })),
+    petId : z.string().pipe(z.number({ invalid_type_error: "El id no es válido", required_error: "El id es requerido" })),
+});
+
+export type INewLinkRequest = z.infer<typeof ZNewLinkRequest>;
