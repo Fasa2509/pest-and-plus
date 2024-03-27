8a5c5668a7abe1838d9a16da49bea75a319ddf8a
Miguel Fasanella <miguellfasanellap@gmail.com>
2024-03-27 02:31:28 -0400

    fix: agregado vercel a la configuracion de astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 // import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  }),
+  // adapter: node({
+  //   mode: "standalone"
+  // }),
+  adapter: vercel(),
   integrations: [preact()]
   // image: {
   //   domains: [""],
