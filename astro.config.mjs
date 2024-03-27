41481c84d5897d9c48789ef45bbde0f040733430
Miguel Fasanella <miguellfasanellap@gmail.com>
2024-03-27 11:06:12 -0400

    fix: agregado alias de resolver de import de Prisma

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,19 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        ".prisma/client/index-browser": "@prisma/client/index-browser.js"
+      }
+    }
+  },
   output: "server",
+  adapter: vercel(),
+  integrations: [preact()],
   // adapter: node({
   //   mode: "standalone"
   // }),
-  adapter: vercel(),
-  integrations: [preact()]
   // image: {
   //   domains: [""],
   // }
